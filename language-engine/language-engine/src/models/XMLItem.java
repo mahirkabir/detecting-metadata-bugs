88f3cde75bc1ec212044430aed63a846bad9f94e
mahirkabir <asefkabir92@gmail.com>
2022-08-09 21:57:17 -0400

    Language-Engine: Read XMLs

--- language-engine/language-engine/src/models/XMLItem.java
+++ language-engine/language-engine/src/models/XMLItem.java
@@ -1,13 +1,80 @@
 package models;
 
-public class XMLItem {
-    private String nodeContent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    public String getNodeContent() {
-        return this.nodeContent;
+import org.w3c.dom.Node;
+
+import utils.Constants;
+
+public class XMLItem {
+    private Node domNode;
+    private Map<String, String> mapAttr;
+    private Map<String, Boolean> mapChildTags;
+    private String id;
+    private String itemType;
+
+    public XMLItem(String id) {
+        super();
+        this.id = id;
+        this.mapAttr = new HashMap<>();
+        this.mapChildTags = new HashMap<>();
     }
 
-    public void setNodeContent(String content) {
-        this.nodeContent = content;
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public Node getDomNode() {
+        return this.domNode;
+    }
+
+    public void setDomNode(Node domNode) {
+        this.domNode = domNode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<String, String> getMapAttr() {
+        return mapAttr;
+    }
+
+    public void setMapAttr(Map<String, String> mapAttr) {
+        this.mapAttr = mapAttr;
+    }
+
+    public void addAttr(String attr, String value) {
+        this.mapAttr.put(attr, attr);
+    }
+
+    public String getAttr(String attr) {
+        if (this.mapAttr.containsKey(attr))
+            return this.mapAttr.get(attr);
+        return Constants.EMPTY_STRING;
+    }
+
+    public Map<String, Boolean> getMapChildTags() {
+        return this.mapChildTags;
+    }
+
+    public void setMapChildTags(Map<String, Boolean> mapChildTags) {
+        this.mapChildTags = mapChildTags;
+    }
+
+    public void addChildTag(String tag) {
+        if (!this.mapChildTags.containsKey(tag))
+            this.mapChildTags.put(tag, true);
     }
 }
--- language-engine/language-engine/src/utils/XMLHelper.java
+++ language-engine/language-engine/src/utils/XMLHelper.java
@@ -6,20 +6,27 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import models.XMLItem;
+
 public class XMLHelper {
     private String xmlFilePath;
+    private DocumentBuilderFactory documentBuilderFactory;
+    private DocumentBuilder documentBuilder;
+    private Document document;
 
     public XMLHelper(String xmlFilePath) {
         super();
         this.xmlFilePath = xmlFilePath;
     }
 
-    public void readXMLs() throws ParserConfigurationException, SAXException, IOException {
+    public void readXML1() throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         Document document = documentBuilder.parse(this.xmlFilePath);
@@ -37,11 +44,38 @@ public class XMLHelper {
                     Node grandchild = grandchildren.item(j);
                     if (grandchild.getNodeType() == Node.ELEMENT_NODE) {
                         System.out.println("\t==>" +
-                                " " + grandchild.getNodeType() +
                                 " " + grandchild.getNodeName());
                     }
                 }
             }
         }
+    }
+
+    public XMLItem readXML() throws SAXException, IOException, ParserConfigurationException {
+        this.documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        this.documentBuilder = this.documentBuilderFactory.newDocumentBuilder();
+        this.document = this.documentBuilder.parse(this.xmlFilePath);
+
+        NodeList nodeList = document.getElementsByTagName("*");
+
+        XMLItem xmlItem = new XMLItem(this.xmlFilePath); // Root xml item's id is the file path
+        xmlItem.setItemType(Constants.NODE_TYPE_ROOT);
+        int totalNumOfChildren = nodeList.getLength();
+        for (int i = 0; i < totalNumOfChildren; ++i) {
+            // Tracking all the tags used in the xml
+            Node child = nodeList.item(i);
+            if (child.getNodeType() == Node.ELEMENT_NODE)
+                xmlItem.addChildTag(child.getNodeName());
+        }
+
+        Node rootNode = nodeList.item(0);
+        NamedNodeMap attMap = rootNode.getAttributes();
+        int totalAttrs = attMap.getLength();
+        for (int i = 0; i < totalAttrs; ++i) {
+            Attr attr = (Attr) attMap.item(i);
+            xmlItem.addAttr(attr.getName(), attr.getValue());
+        }
+
+        return null;
     }
 }
