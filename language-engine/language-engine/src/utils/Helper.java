7492809e0f6732807780dd1940a81183f83d4971
mahirkabir <asefkabir92@gmail.com>
2022-09-05 17:45:40 -0400

    Language-Engine: Add missing typecase method

--- language-engine/language-engine/src/utils/Helper.java
+++ language-engine/language-engine/src/utils/Helper.java
@@ -1,8 +1,16 @@
 package utils;
 
+import models.ClassItem;
 import models.DataResult;
 
 public class Helper {
+    /**
+     * Check if two objects are equal
+     * 
+     * @param firstResult
+     * @param secondResult
+     * @return
+     */
     public static boolean isEqual(DataResult firstResult, DataResult secondResult) {
         if (firstResult.getType().equals("String")) {
             return firstResult.getResult().equals(secondResult.getResult());
@@ -12,5 +20,25 @@ public class Helper {
         // Note: Currently, we do not have equality comparisons for class, method,
         // field, & file type
         return false;
+    }
+
+    /**
+     * Type cast Object to a datatype and return result object
+     * 
+     * @param iteratorType
+     * @param element
+     * @return
+     */
+    public static DataResult typeCastValue(String iteratorType, Object element) {
+        DataResult result = null;
+
+        switch (iteratorType) {
+            case Constants.TYPE_CLASS:
+                ClassItem converted = (ClassItem) element;
+                result = new DataResult<ClassItem>(iteratorType, converted);
+                break;
+        }
+
+        return result;
     }
 }
