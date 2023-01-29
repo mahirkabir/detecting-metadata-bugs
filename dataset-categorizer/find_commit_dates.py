24ff4ba391f7d1c5f3a875ae0de59f8540b1be8b
mahirkabir <asefkabir92@gmail.com>
2023-01-29 12:40:01 +0600

    Rename result to dict_result

--- dataset-categorizer/find_commit_dates.py
+++ dataset-categorizer/find_commit_dates.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     """Find commit dates from commit id"""
     dataset_loc = "D:\Mahir\detecting-metadata-bugs\dataset\projects"
     root = os.getcwd()
-    result = {}
+    dict_result = {}
     with open(os.path.join(root, "unique_commits.txt"), "r", encoding="utf-8", errors="ignore") as reader:
         lines = reader.readlines()
         proj = ""
@@ -27,11 +27,11 @@ if __name__ == "__main__":
             if "Project: " in line:
                 proj = line.replace("Project: ", "")
                 proj_loc = os.path.join(dataset_loc, proj)
-                result[proj] = []
+                dict_result[proj] = []
             else:
                 commit_id = line
                 try:
-                    result, output = helper.execute_cmd(
+                    _, output = helper.execute_cmd(
                         proj_loc, "git show %s" % commit_id)
                     output_lines = output.split("\n")
                     for oline in output_lines:
@@ -40,14 +40,14 @@ if __name__ == "__main__":
                             year, month, day = parts[7], get_month(
                                 parts[4]), parts[5]
 
-                            result[proj].append("%s\t(%s-%s-%s)" %
+                            dict_result[proj].append("%s\t(%s-%s-%s)" %
                                                 (commit_id, year, month, day))
                 except Exception as ex:
                     print("Error. Project: %s. Commit: %s => %s" %
                           (proj, commit_id, str(ex)))
 
     with open(os.path.join(root, "commit_dates.txt"), "w", encoding="utf-8", errors="ignore") as writer:
-        for proj in tqdm(result):
-            writer.write("%s: (%s)\n" % (proj, len(result[proj])))
-            for commit in result[proj]:
+        for proj in tqdm(dict_result):
+            writer.write("%s: (%s)\n" % (proj, len(dict_result[proj])))
+            for commit in dict_result[proj]:
                 writer.write("%s\n" % commit)
