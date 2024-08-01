ac3339f3a3a542bb6aa04aacea36066fdb635915
Mahir92 <asefkabir92@gmail.com>
2024-08-01 00:36:01 -0400

    Extend the keywords to make it more specific (e.g. - @RunWith)

--- dataset-categorizer/bug-injection/synthetic_dataset_maker.py
+++ dataset-categorizer/bug-injection/synthetic_dataset_maker.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     # Define the paths
     dataset_folder = "D:\\Mahir\\detecting-metadata-bugs\\dataset\\all_projects"
     log_file_path = "keyword_file_match.txt"
+    project_keyword_log_file_path = "project_keywords.txt"
     
 
     # Get a list of all projects in the dataset folder
@@ -35,16 +36,20 @@ if __name__ == "__main__":
 
     keywords = {
         "@ImportResource": "java", "ApplicationContext": "java",
-        ".getBean(": "java", "</bean>": "xml", "<property": "xml",
+        ".getBean(": "java", "<bean": "xml", "<property": "xml",
         "constructor-arg": "xml", "-method=": "xml",
-        "ClassPathXmlApplicationContext": "java"
+        "ClassPathXmlApplicationContext": "java",
+        "org.junit": "java", "@RunWith(": "java", "@RunWith(Suite.class)": "java",
+        "@Parameters": "java", "@Parameterized.Parameters": "java"
     }
-    limit = 12
+    limit = 831 # infinity
     selected_projects = {}
+    project_keywords = {}
     needed_projects_cnt = int(limit * len(keywords))
 
     # Check projects until we find `limit` of each keyword or run out of projects
     for project in tqdm(all_projects):
+        project_keywords[project] = []
         for keyword in keywords:
             if keyword in selected_projects and len(selected_projects[keyword]) == limit:
                 continue
@@ -53,6 +58,7 @@ if __name__ == "__main__":
                 if file_path:
                     if keyword not in selected_projects: selected_projects[keyword] = []
                     selected_projects[keyword].append((project, file_path))
+                    project_keywords[project].append(keyword)
                     needed_projects_cnt -= 1
                     
                 if needed_projects_cnt == 0:
@@ -68,5 +74,18 @@ if __name__ == "__main__":
             if keyword in selected_projects:
                 for project, file_path in selected_projects[keyword]:
                     log_file.write(f"{keyword}\t{project}\t{file_path}\n")
-
+                    
     print(f"Log file created at {log_file_path} with selected projects.")
+
+    # Write the keywords for the projects in file
+    with open(project_keyword_log_file_path, "w") as keyword_log:
+        for project in all_projects:
+            keyword_str = ""
+            if project in project_keywords and len(project_keywords[project]) != 0:
+                for keyword in project_keywords[project]:
+                    if keyword_str == "": keyword_str = keyword
+                    else: keyword_str = f"{keyword_str}, {keyword}"
+            project = project.split("\\")[-1]
+            keyword_log.write(f"{project}\t{keyword_str}\n")
+                    
+    print(f"Project Keyword Log file created at {project_keyword_log_file_path} with selected projects.")
