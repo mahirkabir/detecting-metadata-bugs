0a10fb5e5e6205734cd003a0b4a65043cc0322da
Mahir92 <asefkabir92@gmail.com>
2024-08-15 11:20:22 -0400

    Real-Bug: Prune data and prepare rules to run for the commits

--- dataset-categorizer/keyword-file-commit-finder/keyword_file_commit_finder.py
+++ dataset-categorizer/keyword-file-commit-finder/keyword_file_commit_finder.py
@@ -56,6 +56,41 @@ def collect_file_commits(repo_path, filepath):
     
     return commits
     
+    
+def merge(list1, list2):
+    """Merge two lists removing duplicates"""
+    dict = {}
+    for elm in list1: dict[elm] = True
+    for elm in list2: dict[elm] = True
+    res = []
+    for elm in dict: res.append(elm)
+    return res
+
+def get_keyword_rules(keywords):
+    """Get the rules to run for the keywords"""
+    dict_keyword_rules = {
+        "@ImportResource": ["importXMLIntoAnnotation"], 
+        "ApplicationContext": ["beanExists"],
+        ".getBean(": ["beanExists"], 
+        "<bean": ["beanClassExists", "dupBeansExist"], 
+        "<property": ["setterMethod"],
+        "constructor-arg": ["constructorArgumentField", "constructorArgumentFieldType", "constructorIndexOutOfBound"], 
+        "-method=": ["methodExists"],
+        "ClassPathXmlApplicationContext": ["xmlPathCheck"],
+        "org.junit": ["runwithNoTest"], 
+        "@RunWith(": ["runwithNoTest", "runwithNoParameters", "suiteclassesNoRunwith"], 
+        "@RunWith(Suite.class)": ["suiteclassesNoTest"],
+        "@Parameters": ["runwithNoParameters", "testParamsNotIterable", "runwithNoTest"], 
+        "@Parameterized.Parameters": ["runwithNoParameters", "testParamsNotIterable", "runwithNoTest"]
+    }
+    
+    rules = []
+    for keyword in keywords:
+        if keyword in dict_keyword_rules:
+            keyword_rules = dict_keyword_rules[keyword]
+            rules = merge(rules, keyword_rules)
+    
+    return rules
 
 if __name__ == "__main__":
     """Find commits of randomly selected files based on keywords for realbug-related experiments"""
@@ -64,6 +99,32 @@ if __name__ == "__main__":
     randomly_selected_repos = get_preselected_random_repos("random_real_bug_repos.txt")
     project_keyword_output_file_path = "project_keyword_file_commits.json"
     project_keyword_output_file_path_sheet = "project_keyword_output_file_path.txt"
+    
+    already_processed_in_phase1 = [
+        "angular-js-spring-mybatis",
+        "basis-webapp-sample",
+        "dspace-rest",
+        "MongoDBSpringRest",
+        "ShcUtils",
+        "spring-vaadin",
+        "cv-web",
+        "biyam_repository",
+        "generica",
+        "collection-manager",
+        "Kognitywistyka",
+        "LIBRARY",
+        "enterprise-routing-system",
+        "jarvis",
+        "I377-esk",
+        "FileExplorer",
+        "E2-Demo",
+        "aioweb",
+        "CWISE-Portal"
+    ]
+    
+    for repo in already_processed_in_phase1:
+        if repo in randomly_selected_repos:
+            randomly_selected_repos.remove(repo)
     
     keywords = {
         "@ImportResource": "java", "ApplicationContext": "java",
@@ -106,19 +167,35 @@ if __name__ == "__main__":
             print(f"Error saving data to JSON: {str(ex)}")
     
     # Save the dictionary data in text file
+    output_dict = {}
+    repo_count = {}
+    for repo in tqdm(randomly_selected_repos):
+        if repo in repo_keyword_file_commit:
+            output_dict[repo] = {}
+            repo_count[repo] = 0
+            for filepath in repo_keyword_file_commit[repo]:
+                keywords = repo_keyword_file_commit[repo][filepath]["keywords"]
+                for commit in repo_keyword_file_commit[repo][filepath]["commits"]:
+                    commit_id = commit["hash"]
+                    commit_date = commit["date"]
+                    commit_message = commit["message"].replace("\n", "  ")
+                    # output = f"{repo}\t{filepath}\t{keywords}\t{commit_id}\t{commit_date}\t{commit_message}\n"
+                    if commit_id not in output_dict[repo]:
+                        output_dict[repo][commit_id] = {}
+                        repo_count[repo] += 1
+                    # output_dict[repo][commit_id] = f"{keywords}" #\t{commit_date}\t{commit_message}"
+                    output_dict[repo][commit_id] = merge(output_dict[repo][commit_id], keywords)
     try:
         with open(project_keyword_output_file_path_sheet, "w", encoding="utf-8", errors="ignore") as sheet_file:
-            for repo in tqdm(randomly_selected_repos):
-                if repo in repo_keyword_file_commit:
-                    for filepath in repo_keyword_file_commit[repo]:
-                        keywords = ",".join(repo_keyword_file_commit[repo][filepath]["keywords"])
-                        for commit in repo_keyword_file_commit[repo][filepath]["commits"]:
-                            commit_id = commit["hash"]
-                            commit_date = commit["date"]
-                            commit_message = commit["message"].replace("\n", "</br>")
-                            # output = f"{repo}\t{filepath}\t{keywords}\t{commit_id}\t{commit_date}\t{commit_message}\n"
-                            output = f"{repo}\t{filepath}\t{commit_id}\t{commit_date}\t{commit_message}\n"
-                            sheet_file.write(output)
+            for repo in output_dict:
+                for commit_id in output_dict[repo]:
+                    # for filepath in output_dict[repo][commit_id]:
+                    #     # output_dict[repo][commit_id][filepath].replace("\n", "</br>").strip()
+                    #     # sheet_file.write(f"{repo}\t{commit_id}\t{filepath}\t{output_dict[repo][commit_id][filepath]}\n")
+                    #     sheet_file.write(f"{repo}\t{commit_id}\t{repo_count[repo]}\t{output_dict[repo][commit_id]}\n")
+                    rules = get_keyword_rules(output_dict[repo][commit_id])
+                    rules_str = ", ".join(rules)
+                    sheet_file.write(f"{repo}\t{commit_id}\t{repo_count[repo]}\t{rules_str}\n")
                             
     except Exception as ex:
         print(f"Error saving data to file: {str(ex)}")
